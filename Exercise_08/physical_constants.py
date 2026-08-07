f40c95a9d5c4430754ad9471c15779f3ff645b50
Bradley Canty <BradleyScottCanty@gmail.com>
2026-08-07 18:10:02 -0400

    Delete Exercise_08 directory

--- Exercise_08/physical_constants.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-physical_constants.py
-"""
-
-G = 6.674E-11 #m^3/(kg*s^2), universal gravitational constant
-m_earth = 5.972E24 #kg, mass of earth
-r_earth = 6371E3 #m, maan radius of earth
