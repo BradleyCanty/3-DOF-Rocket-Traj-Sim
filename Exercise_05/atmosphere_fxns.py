789b1af5358c65538446243e05a22cc80a979e3b
Bradley Canty <BradleyScottCanty@gmail.com>
2026-08-06 21:28:16 -0400

    Add files via upload

---
+++ Exercise_05/atmosphere_fxns.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+atmosphere_fxns.py
+
+Description:
+    Compute air density as a function of altitude using the isothermal barometric
+    formula. Compare this model to tabulated U.S. Standard Atmosphere data
+    (https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html#gsc.tab=0)
+    to verify that it's suitable for our use.
+    
+    The isothermal barometric formula is given by:
+    rho(h) = rho0 * exp(-h/H)
+    where
+    rho0 = 1.225 [kg/m^3] = air density at sea level
+    h = altitude above sea level
+    H = scale height of the atmosphere = 8500 [m]
+    
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+img_dir = os.getcwd() + r'\Images'
+#If the Image directory does not exist, create it
+if not os.path.exists(img_dir):
+    os.makedirs(img_dir)
+
+#Form the isothermal barometric formula
+rho0 = 1.225 #sea level air density
+H = 8500 #scale height
+rho_air = lambda h: rho0 * np.exp(-h/H) #kg/m^3, isothermal barometric formula
+
+#U.S. Standard Atmosphere altitude data from 0 to 80[km]
+h_data = np.array([0,
+1000,
+2000,
+3000,
+4000,
+5000,
+6000,
+7000,
+8000,
+9000,
+10000,
+15000,
+20000,
+25000,
+30000,
+40000,
+50000,
+60000,
+70000,
+80000
+])
+
+#U.S. Standard Atmosphere air density data corresponding to altitudes from 0 to 80[km]
+rho_air_data = np.array([1.225,
+1.112,
+1.007,
+0.9093,
+0.8194,
+0.7364,
+0.6601,
+0.59,
+0.5258,
+0.4671,
+0.4135,
+0.1948,
+0.08891,
+0.04008,
+0.01841,
+0.003996,
+0.001027,
+0.0003097,
+0.00008283,
+0.00001846
+])
+
+if __name__ == '__main__':
+    #Compare the isothermal barometric formula with the U.S. Standard Atmosphere data
+    h = np.linspace(0,80000,101)
+    plt.figure()
+    plt.plot(rho_air(h),h,'b',label='isothermal barometric formula')
+    plt.plot(rho_air_data,h_data,'r.',label='U.S. Std. Atmos. data')
+    plt.title(r'Air Density vs Altitude')
+    plt.xlabel(r'air density [$kg/m^3$]')
+    plt.ylabel('altitude [m]')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(img_dir + '/air_density_vs_altitude.png', dpi = 600)
+    plt.show()
+    
+    print(f"Plots output to:\n{img_dir}")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Exercise_05/rocket_traj_sim_ex5.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+rocket_traj_sim_ex5.py
+
+Description:
+    Implement an altitude-varying air density in the 1D rocket trajectory simulation.
+    
+    The isothermal barometric formula is given by:
+    rho(h) = rho0 * exp(-h/H)
+    where
+    rho0 = 1.225 [kg/m^3] = air density at sea level
+    h = altitude above sea level
+    H = scale height of the atmosphere = 8500 [m]
+    
+    To get started, create a new file named 'atmosphere_fxns.py' and implement the 
+    formula in a function. Then, check its accuracy by comparing it to 
+    tabulated U.S. Standard Atmosphere data (https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html#gsc.tab=0)
+    
+    Then, import the function into the 'math_fxns.py' file and use it in the system 
+    of ODEs.
+    
+    Finally, plot the rocket's acceleration, velocity, and position as a function 
+    of time.
+    
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from math_fxns import get_rocket_state
+import os
+
+img_dir = os.getcwd() + r'\Images'
+#If the Image directory does not exist, create it
+if not os.path.exists(img_dir):
+    os.makedirs(img_dir)
+
+r_earth = 6371000 #m, mean earth radius
+
+#Initial conditions
+ry0 = r_earth
+vy0 = 0 
+
+t = np.linspace(0, 500, 301)
+
+#Assume uniform time step
+dt = t[1] - t[0]
+
+tt,ry,vy,ay = get_rocket_state(ry0,vy0,t,'RK4')
+
+plt.figure()
+plt.plot(tt,ay,'r')
+plt.title(r'$a_y$ vs Time for V2 Rocket (g = f(h), $\rho_{air} = f(h)\ [kg/m^3]$)')
+plt.xlabel('time [s]')
+plt.ylabel('acceleration [m/s^2]')
+plt.grid(True)
+plt.savefig(img_dir + '/V2_rocket_vs_time.png', dpi = 600)
+plt.show()
+
+plt.figure()
+plt.plot(tt,vy,'r')
+plt.title(r'$v_y$ vs Time (g = f(h), $\rho_{air} = f(h)\ [kg/m^3]$)')
+plt.xlabel('time [s]')
+plt.ylabel('velocity [m/s]')
+plt.grid(True)
+plt.savefig(img_dir + '/V2_rocket_vs_time.png', dpi = 600)
+plt.show()
+
+plt.figure()
+plt.plot(tt,ry-r_earth,'r')
+plt.title(r'Altitude vs Time (g = f(h), $\rho_{air} = f(h)\ [kg/m^3]$)')
+plt.xlabel('time [s]')
+plt.ylabel('Altitude [m]]')
+plt.grid(True)
+plt.savefig(img_dir + '/V2_rocket_vs_time.png', dpi = 600)
+plt.show()
+
+print(f"Plots output to:\n{img_dir}")
+
+
